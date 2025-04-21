99b8b5d583f2d2d04690ee1b31d5045427b43927
oumaimansir <oumaima.nsir2001@gmail.com>
2025-04-21 22:00:39 +0000

    kubernetes

--- salle_service/app/database.py
+++ salle_service/app/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-SQLALCHEMY_DATABASE_URL = "postgresql://myuser:123456@postgres/authdb"
+import os
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://myuser:123456@postgres/authdb")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
